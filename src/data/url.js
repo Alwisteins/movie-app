03145ec397b1e3cc7cb37fadf47c6a3c3f6dc2ab
unknown <alwyyourbae@gmail.com>
2024-07-01 07:46:19 +0700

    remove vite proxy

--- src/data/url.js
+++ src/data/url.js
@@ -1,9 +1,9 @@
 const url = {
-  default: "/api",
-  nowPlaying: "/api/now_playing?page=1",
-  popular: "/api/popular?page=1",
-  topRated: "/api/top_rated?page=1",
-  upcoming: "/api/upcoming?page=1",
+  default: "https://api.themoviedb.org/3/movie",
+  nowPlaying: "https://api.themoviedb.org/3/movie/now_playing?page=1",
+  popular: "https://api.themoviedb.org/3/movie/popular?page=1",
+  topRated: "https://api.themoviedb.org/3/movie/top_rated?page=1",
+  upcoming: "https://api.themoviedb.org/3/movie/upcoming?page=1",
 };
 
 export default url;
